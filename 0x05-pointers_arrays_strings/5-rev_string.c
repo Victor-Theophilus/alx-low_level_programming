52e815ce2e4e881cc730f27f61adae1da4170584
Victor <Theophiluspatrick2@gmail.com>
2022-09-19 15:43:47 -0700

    4and5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	 int l, i;
+	 char ch;
+
+	  /*find string length without null char*/
+	  for (l = 0; s[l] != '\0'; ++l)
+
+		  /*swap the string by looping to half the string*/
+		  for (i = 0; i < l / 2; ++i)
+		  {
+			  ch = s[i];
+			  s[i] = s[l - 1 - i]; /*-1 because the array star
+						 ts from 0*/
+			  s[l - 1 - i] = ch;
+		  }
+}
